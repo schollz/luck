352fa1e4dba2408cbb8d765a5e85d81a5d20bcf9
Zack Scholl <zack.scholl@gmail.com>
2016-08-23 14:47:17 -0400

    Updated

--- analyze-nba-data.m
+++ analyze-nba-data.m
@@ -1,3 +1,5 @@
+a = importdata('nbaallelo-output.txt');
+
 start = min(a(:,1));
 rangeN = min(a(:,1)):max(a(:,1))+1;
 dataW = zeros(size(rangeN));
@@ -7,3 +9,6 @@ for i=1:size(a,1)
     dataW(a(i,1)-start+1) = dataW(a(i,1)-start+1) + a(i,2);
 end
 
+plot(rangeN,dataW./dataN)
+ylabel('Probability of winning')
+xlabel('Difference in Elo score')
